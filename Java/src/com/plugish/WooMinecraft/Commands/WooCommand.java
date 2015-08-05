8467c1b053ed5be0de7613cd2601130b3fa51cf4
TekkitCommando <tekkitcommando@gmail.com>
2015-08-05 12:37:12 -0500

    Merge Development (Had to do it manually ;-;)

--- Java/src/com/plugish/WooMinecraft/Commands/WooCommand.java
+++ Java/src/com/plugish/WooMinecraft/Commands/WooCommand.java
@@ -5,52 +5,57 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
-import java.math.BigInteger;
 import java.security.SecureRandom;
-import java.util.logging.Logger;
 
-/**
- * Created by ethan on 7/30/2015.
- */
 public class WooCommand implements CommandExecutor {
 
-    private Logger log;
+    public static WooMinecraft plugin = WooMinecraft.instance;
 
-    public boolean onCommand(CommandSender sender, Command cmd, String Label, String[] args) {
-        Player player = (Player) sender;
-        if(cmd.getName().equalsIgnoreCase("Woo")) {
-            if(args.length == 0) {
-                if(sender tanceof Player) {
-                    player.sendMessage(ChatColor.BLUE + "[Woo]" + ChatColor.AQUA + " Available Commands: Woo [Register | Reload | Check]");
+    @Override
+    public boolean onCommand(CommandSender s, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("woo") && args.length == 0) {
+            if (s.hasPermission("woo.admin") || s.isOp()) {
+                s.sendMessage("Available Commands: /woo [register , reload , check]");
+            } else {
+                s.sendMessage("You Don't Have Permission for that Command!");
+            }
+        } else if(command.getName().equalsIgnoreCase("woo") && args.length == 1) {
+            if(args[0].equalsIgnoreCase("reload")) {
+                if(s.hasPermission("woo.admin") || s.isOp()) {
+                    plugin.reloadConfig();
+                    s.sendMessage("[Woo] Config Reloaded");
                 } else {
-                    log.info("[Woo] Available Commands: Woo [Register | Reload | Check]");
+                    s.sendMessage("You Don't Have Permission for that Command!");
                 }
-            } else if(sender.hasPermission("woo.reload") || sender.isOp()) {
-                if(args[0].equalsIgnoreCase("reload")) {
-                    // This will be fixed up soon, This is only or temporary use
-                    WooMinecraft.plugin.reloadConfig();
-                    if(sender instanceof Player) {
-                        player.sendMessage(ChatColor.BLUE + "[Woo]" + ChatColor.AQUA + " Config Reloaded!");
-                    } else {
-                        log.info("[Woo] Config Reloaded!");
-                    }
-                }
-            } else if(sender.hasPermission("woo.register") || sender.isOp()) {
-                if(args[0].equalsIgnoreCase("register")) {
+            } else if(args[0].equalsIgnoreCase("register")) {
+                if(s.hasPermission("woo.admin") || s.isOp()) {
                     SecureRandom random = new SecureRandom();
+                    byte[] randomBytes = new byte[32];
+                    random.nextBytes(randomBytes);
+
                     String key = "";
-                    if(WooMinecraft.c.getString(WooMinecraft.urlPath+",key") == "") {
-                        key = new BigInteger(130,random).toString(32);
-                        WooMinecraft.c.set(WooMinecraft.urlPath+".key", key);
-                        // this is gonna give an error ;)
-                        saveConfig();
+                    if (plugin.c.getString(plugin.urlPath + ".key") == "") {
+                        key = String.valueOf(randomBytes);
+                        plugin.c.set(plugin.urlPath + ".key", key);
+                        plugin.saveConfig();
+                        s.sendMessage(ChatColor.AQUA + "[WOO]" + ChatColor.RED + " KEY: " + key);
+                        s.sendMessage(ChatColor.RED + "Copy this key and put it in your WooMinecraft options panel in WordPress");
+                    } else {
+                        key = plugin.c.getString(plugin.urlPath + ".key");
                     }
+                } else {
+                    s.sendMessage("You Don't Have Permission for that Command!");
+                }
+            } else if(args[0].equalsIgnoreCase("check")) {
+                if(s.hasPermission("woo.admin") || s.isOp()) {
+                    // well we need to run the check then don't we?
+                    plugin.check();
+                } else {
+                    s.sendMessage("You Don't Have Permission for that Command!");
                 }
             }
         }
         return true;
     }
-
 }
