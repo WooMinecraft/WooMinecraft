79856e9f52c7c00e6299eaafdf08be1dfcbb84b2
TekkitCommando <tekkitcommando@gmail.com>
2015-08-08 07:57:28 -0500

    Add New Message Theme

--- Java/src/com/plugish/WooMinecraft/Commands/WooCommand.java
+++ Java/src/com/plugish/WooMinecraft/Commands/WooCommand.java
@@ -12,22 +12,23 @@ import java.util.UUID;
 public class WooCommand implements CommandExecutor {
 
     public static WooMinecraft plugin = WooMinecraft.instance;
+    public static String Theme = ChatColor.DARK_PURPLE + "[" + ChatColor.WHITE + "Woo" + ChatColor.DARK_PURPLE + "] " + ChatColor.DARK_PURPLE + "";
 
     @Override
     public boolean onCommand(CommandSender s, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("woo") && args.length == 0) {
             if (s.hasPermission("woo.admin") || s.isOp()) {
-                s.sendMessage("Available Commands: /woo [register , reload , check]");
+                s.sendMessage( Theme + " Available Commands: /woo [register , reload , check]");
             } else {
-                s.sendMessage("You Don't Have Permission for that Command!");
+                s.sendMessage(Theme + " You Don't Have Permission for that Command!");
             }
         } else if(command.getName().equalsIgnoreCase("woo") && args.length == 1) {
             if(args[0].equalsIgnoreCase("reload")) {
                 if(s.hasPermission("woo.admin") || s.isOp()) {
                     plugin.reloadConfig();
-                    s.sendMessage("[Woo] Config Reloaded");
+                    s.sendMessage(Theme + " Config Reloaded");
                 } else {
-                    s.sendMessage("You Don't Have Permission for that Command!");
+                    s.sendMessage(Theme + " You Don't Have Permission for that Command!");
                 }
             } else if(args[0].equalsIgnoreCase("register")) {
                 s.sendMessage("[Woo] You have used the register command!");
@@ -37,31 +38,31 @@ public class WooCommand implements CommandExecutor {
                     */
                     UUID uuid = UUID.randomUUID();
                     String key = "";
-                    s.sendMessage("[Woo] Created key string == nothing");
+                    s.sendMessage(Theme + " Created key string == nothing");
                     if (plugin.c.getString(plugin.urlPath + ".key") == "") {
-                        s.sendMessage("[Woo] Getting empty config path");
+                        s.sendMessage(Theme + " Getting empty config path");
                         key = uuid.toString().replaceAll("-", "");
-                        s.sendMessage("[Woo] set key = to randomBytes");
+                        s.sendMessage(Theme + " set key = to uuid");
                         plugin.c.set(plugin.urlPath + ".key", key);
-                        s.sendMessage("[Woo] Almost There!");
+                        s.sendMessage(Theme + " Almost There!");
                         plugin.saveConfig();
-                        s.sendMessage("[Woo] Saved Config!");
-                        s.sendMessage(ChatColor.AQUA + "[WOO]" + ChatColor.RED + " KEY: " + key);
-                        s.sendMessage(ChatColor.RED + "Copy this key and put it in your WooMinecraft options panel in WordPress");
+                        s.sendMessage(Theme + " Saved Config!");
+                        s.sendMessage(Theme + " Key: " + key);
+                        s.sendMessage(Theme + " Copy this key and put it in your WooMinecraft options panel in WordPress");
                     } else {
                         key = plugin.c.getString(plugin.urlPath + ".key");
-                        s.sendMessage("[Woo] key already set");
+                        s.sendMessage(Theme + " key already set");
                     }
                 } else {
-                    s.sendMessage("You Don't Have Permission for that Command!");
+                    s.sendMessage(Theme + " You Don't Have Permission for that Command!");
                 }
             } else if(args[0].equalsIgnoreCase("check")) {
                 if(s.hasPermission("woo.admin") || s.isOp()) {
                     // well we need to run the check then don't we?
                     plugin.check();
-                    s.sendMessage("[Woo] Checked Purchases!");
+                    s.sendMessage(Theme + " Checked Purchases!");
                 } else {
-                    s.sendMessage("You Don't Have Permission for that Command!");
+                    s.sendMessage(Theme + " You Don't Have Permission for that Command!");
                 }
             }
         }
