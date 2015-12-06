b8d40e724a483e64a01c19e45622c5c2b0d365b3
Jay Wood <jerry@webdevstudios.com>
2015-12-05 22:44:52 -0500

    Small cleanup of BukkitRunner - still needs some things

--- src/main/java/com/plugish/WooMinecraft/BukkitRunner.java
+++ src/main/java/com/plugish/WooMinecraft/BukkitRunner.java
@@ -7,11 +7,11 @@ public class BukkitRunner extends BukkitRunnable {
 	public static WooMinecraft plugin = WooMinecraft.instance;
 	
 	public BukkitRunner(WooMinecraft plugin){
-		this.plugin = plugin;
+		plugin = plugin;
 	}
 	
 	public void run(){
-		this.plugin.check();
+		plugin.check();
 	}
 
 }
