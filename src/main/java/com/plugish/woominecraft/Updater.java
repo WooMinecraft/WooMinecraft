4bc3927322527d6b61a4263e24bebb5ab4de7ebf
Jay Wood <jerry@webdevstudios.com>
2015-12-10 22:32:16 -0500

    Clean up code formatting

--- src/main/java/com/plugish/woominecraft/Updater.java
+++ src/main/java/com/plugish/woominecraft/Updater.java
@@ -17,39 +17,41 @@ import java.util.zip.ZipFile;
  */
 
 public class Updater {
-    public static WooMinecraft plugin = WooMinecraft.instance;
+	public static WooMinecraft plugin = WooMinecraft.instance;
 
-    private String name = "Woo Minecraft Update";
-    private int byteSize = 0;
+	private String name = "Woo Minecraft Update";
+	private int byteSize = 0;
 
-    private byte data[];
-    private int downloadBytes = 0;
-    private boolean startedDownload = false;
-    private URL downloadURL;
+	private byte data[];
+	private int downloadBytes = 0;
+	private boolean startedDownload = false;
+	private URL downloadURL;
 
-    private long startTime;
+	private long startTime;
 
-    public Updater(){}
-    public Updater(String name, String URL) {
-        this.name = name;
-        try {
-            downloadURL = new URL("http://github.com/WooMinecraft/WooMinecraft/releases/tag");
-        } catch (MalformedURLException e) {
-            plugin.getLang("log.update_bad_link");
-        }
-        this.byteSize = (int) this.getSizeFromSite();
-    }
+	public Updater() {
+	}
 
-    public long getSizeFromSite() {
-        long size = -1;
-        if(downloadURL == null) return size;
-        try {
-            URLConnection conn = downloadURL.openConnection();
-            size = conn.getContentLengthLong();
-            conn.getInputStream().close();
-            } catch(IOException e) {
-            System.out.println(e);
-            }
-        return size;
-    }
+	public Updater( String name, String URL ) {
+		this.name = name;
+		try {
+			downloadURL = new URL( "http://github.com/WooMinecraft/WooMinecraft/releases/tag" );
+		} catch ( MalformedURLException e ) {
+			plugin.getLang( "log.update_bad_link" );
+		}
+		this.byteSize = ( int ) this.getSizeFromSite();
+	}
+
+	public long getSizeFromSite() {
+		long size = -1;
+		if ( downloadURL == null ) return size;
+		try {
+			URLConnection conn = downloadURL.openConnection();
+			size = conn.getContentLengthLong();
+			conn.getInputStream().close();
+		} catch ( IOException e ) {
+			System.out.println( e );
+		}
+		return size;
+	}
 }
