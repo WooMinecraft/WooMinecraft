b933490e0c225a927ca8cc364a80e189a5ed4951
JayWood <jjwood2004@gmail.com>
2019-02-11 15:13:42 -0500

    Ensure the key stays private, do not log to system logs.

--- src/main/java/com/plugish/woominecraft/WooMinecraft.java
+++ src/main/java/com/plugish/woominecraft/WooMinecraft.java
@@ -22,6 +22,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitScheduler;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
@@ -247,7 +248,14 @@ public final class WooMinecraft extends JavaPlugin {
 	 */
 	private String getPendingOrders() throws Exception {
 		URL baseURL = getSiteURL();
-		BufferedReader in = new BufferedReader( new InputStreamReader( baseURL.openStream() ) );
+		BufferedReader in;
+		try {
+			in = new BufferedReader(new InputStreamReader(baseURL.openStream()));
+		} catch( FileNotFoundException e ) {
+			String msg = e.getMessage().replace( getConfig().getString( "key" ), "privateKey" );
+			throw new FileNotFoundException( msg );
+		}
+
 		StringBuilder buffer = new StringBuilder();
 
 		// Walk over each line of the response.
