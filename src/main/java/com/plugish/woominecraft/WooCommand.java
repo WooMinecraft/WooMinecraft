96c1d34b23acfc51afd315495ed4ea3f4c0355d8
Marco Paggioro <marco.paggioro@gmail.com>
2021-03-20 00:44:14 +0100

    Make the check of the order in async without blocking the main thread + CQI

--- src/main/java/com/plugish/woominecraft/WooCommand.java
+++ src/main/java/com/plugish/woominecraft/WooCommand.java
@@ -7,44 +7,40 @@ import org.bukkit.command.CommandSender;
 
 public class WooCommand implements CommandExecutor {
 
-	public static WooMinecraft plugin = WooMinecraft.instance;
-	private static String chatPrefix = ChatColor.DARK_PURPLE + "[" + ChatColor.WHITE + "WooMinecraft" + ChatColor.DARK_PURPLE + "] " + ChatColor.DARK_PURPLE + "";
+    public static WooMinecraft plugin = WooMinecraft.instance;
+    private static final String chatPrefix = ChatColor.DARK_PURPLE + "[" + ChatColor.WHITE + "WooMinecraft" + ChatColor.DARK_PURPLE + "] " + ChatColor.DARK_PURPLE + "";
 
-	@Override
-	public boolean onCommand( CommandSender sender, Command command, String label, String[] args ) {
-		if ( command.getName().equalsIgnoreCase( "woo" ) && args.length == 0 ) {
-			if ( sender.hasPermission( "woo.admin" ) || sender.isOp() ) {
-				sender.sendMessage( chatPrefix + " " + plugin.getLang( "general.avail_commands" ) + ": /woo check" );
-			} else {
-				sender.sendMessage( chatPrefix + " " + plugin.getLang( "general.not_authorized" ) );
-			}
-		} else if ( command.getName().equalsIgnoreCase( "woo" ) && args.length == 1 ) {
-			if ( args[ 0 ].equalsIgnoreCase( "check" ) ) {
-				if ( sender.hasPermission( "woo.admin" ) || sender.isOp() ) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("woo") && args.length == 0) {
+            if (sender.hasPermission("woo.admin") || sender.isOp()) {
+                sender.sendMessage(chatPrefix + " " + plugin.getLang("general.avail_commands") + ": /woo check");
+            } else {
+                sender.sendMessage(chatPrefix + " " + plugin.getLang("general.not_authorized"));
+            }
+        } else if (command.getName().equalsIgnoreCase("woo") && args.length == 1) {
+            if (args[0].equalsIgnoreCase("check")) {
+                if (sender.hasPermission("woo.admin") || sender.isOp()) {
 
-					try {
-						String msg;
-						boolean checkResults = plugin.check();
+                    plugin.check().thenAccept((result) -> {
+                        String msg;
+                        if (!result) {
+                            msg = chatPrefix + " " + plugin.getLang("general.none_avail");
+                        } else {
+                            msg = chatPrefix + " " + plugin.getLang("general.processed");
+                        }
 
-						if ( !checkResults ) {
-							msg = chatPrefix + " " + plugin.getLang( "general.none_avail" );
-						} else {
-							msg = chatPrefix + " " + plugin.getLang( "general.processed" );
-						}
-
-						sender.sendMessage( msg );
-					} catch ( Exception e ) {
-						plugin.getLogger().warning( e.getMessage() );
-						e.printStackTrace();
-					}
-				} else {
-					String msg = plugin.getLang( "general.not_authorized" ).replace( "&", "\u00A7" );
-					sender.sendMessage( msg );
-				}
-			} else {
-				sender.sendMessage( "Usage: /woo check" );
-			}
-		}
-		return true;
-	}
+                        // We need to do it in sync
+                        plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () -> sender.sendMessage(msg));
+                    });
+                } else {
+                    String msg = plugin.getLang("general.not_authorized").replace("&", "\u00A7");
+                    sender.sendMessage(msg);
+                }
+            } else {
+                sender.sendMessage("Usage: /woo check");
+            }
+        }
+        return true;
+    }
 }
--- src/main/java/com/plugish/woominecraft/WooMinecraft.java
+++ src/main/java/com/plugish/woominecraft/WooMinecraft.java
@@ -3,9 +3,9 @@
  * Author:	   Jerry Wood
  * Author URI: http://plugish.com
  * License:	   GPLv2
- * 
+ *
  * Copyright 2014 All rights Reserved
- * 
+ *
  */
 package com.plugish.woominecraft;
 
@@ -16,6 +16,7 @@ import com.plugish.woominecraft.pojo.WMCPojo;
 import com.plugish.woominecraft.pojo.WMCProcessedOrders;
 import okhttp3.*;
 import org.bukkit.Bukkit;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -27,279 +28,292 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public final class WooMinecraft extends JavaPlugin {
 
-	static WooMinecraft instance;
+    static WooMinecraft instance;
 
-	private YamlConfiguration l10n;
+    private YamlConfiguration l10n;
 
-	@Override
-	public void onEnable() {
-		instance = this;
-		YamlConfiguration config = (YamlConfiguration) getConfig();
+    @Override
+    public void onEnable() {
+        instance = this;
+        YamlConfiguration config = (YamlConfiguration) getConfig();
 
-		// Save the default config.yml
-		try{
-			saveDefaultConfig();
-		} catch ( IllegalArgumentException e ) {
-			getLogger().warning( e.getMessage() );
-		}
+        // Save the default config.yml
+        try {
+            saveDefaultConfig();
+        } catch (IllegalArgumentException e) {
+            getLogger().warning(e.getMessage());
+        }
 
-		String lang = getConfig().getString("lang");
-		if ( lang == null ) {
-			getLogger().warning( "No default l10n set, setting to english." );
-		}
+        String lang = getConfig().getString("lang");
+        if (lang == null) {
+            getLogger().warning("No default l10n set, setting to english.");
+        }
 
-		// Load the commands.
-		getCommand( "woo" ).setExecutor( new WooCommand() );
+        // Load the commands.
+        PluginCommand wooCommand = getCommand("woo");
+        if (wooCommand != null) {
+            wooCommand.setExecutor(new WooCommand());
+        } else {
+            getLogger().warning("Cannot register the 'woo' command");
+        }
 
-		// Log when plugin is initialized.
-		getLogger().info( this.getLang( "log.com_init" ));
 
-		// Setup the scheduler
-		BukkitRunner scheduler = new BukkitRunner(instance);
-		scheduler.runTaskTimerAsynchronously( instance, config.getInt( "update_interval" ) * 20, config.getInt( "update_interval" ) * 20 );
+        // Log when plugin is initialized.
+        getLogger().info(this.getLang("log.com_init"));
 
-		// Log when plugin is fully enabled ( setup complete ).
-		getLogger().info( this.getLang( "log.enabled" ) );
-	}
+        // Setup the scheduler
+        BukkitRunner scheduler = new BukkitRunner(instance);
+        scheduler.runTaskTimerAsynchronously(instance, config.getInt("update_interval") * 20L, config.getInt("update_interval") * 20L);
 
-	@Override
-	public void onDisable() {
-		// Log when the plugin is fully shut down.
-		getLogger().info( this.getLang( "log.com_init" ) );
-	}
+        // Log when plugin is fully enabled ( setup complete ).
+        getLogger().info(this.getLang("log.enabled"));
+    }
 
-	/**
-	 * Helper method to get localized strings
-	 *
-	 * Much better than typing this.l10n.getString...
-	 * @param path Path to the config var
-	 * @return String
-	 */
-	String getLang(String path) {
-		if ( null == this.l10n ) {
+    @Override
+    public void onDisable() {
+        // Log when the plugin is fully shut down.
+        getLogger().info(this.getLang("log.com_init"));
+    }
 
-			LangSetup lang = new LangSetup( instance );
-			l10n = lang.loadConfig();
-		}
+    /**
+     * Helper method to get localized strings
+     * <p>
+     * Much better than typing this.l10n.getString...
+     *
+     * @param path Path to the config var
+     * @return String
+     */
+    String getLang(String path) {
+        if (null == this.l10n) {
 
-		return this.l10n.getString( path );
-	}
+            LangSetup lang = new LangSetup(instance);
+            l10n = lang.loadConfig();
+        }
 
-	/**
-	 * Validates the basics needed in the config.yml file.
-	 *
-	 * Multiple reports of user configs not having keys etc... so this will ensure they know of this
-	 * and will not allow checks to continue if the required data isn't set in the config.
-	 *
-	 * @throws Exception Reason for failing to validate the config.
-	 */
-	private void validateConfig() throws Exception {
+        return this.l10n.getString(path);
+    }
 
-		if ( 1 > this.getConfig().getString( "url" ).length() ) {
-			throw new Exception( "Server URL is empty, check config." );
-		} else if ( this.getConfig().getString( "url" ).equals( "http://playground.dev" ) ) {
-			throw new Exception( "URL is still the default URL, check config." );
-		} else if ( 1 > this.getConfig().getString( "key" ).length() ) {
-			throw new Exception( "Server Key is empty, this is insecure, check config." );
-		}
-	}
+    /**
+     * Validates the basics needed in the config.yml file.
+     * <p>
+     * Multiple reports of user configs not having keys etc... so this will ensure they know of this
+     * and will not allow checks to continue if the required data isn't set in the config.
+     *
+     * @throws Exception Reason for failing to validate the config.
+     */
+    private void validateConfig() throws Exception {
 
-	/**
-	 * Gets the site URL
-	 *
-	 * @return URL
-	 * @throws Exception Why the URL failed.
-	 */
-	private URL getSiteURL() throws Exception {
-		return new URL( getConfig().getString( "url" ) + "/wp-json/wmc/v1/server/" + getConfig().getString( "key" ) );
-	}
+        if (1 > this.getConfig().getString("url").length()) {
+            throw new Exception("Server URL is empty, check config.");
+        } else if (this.getConfig().getString("url").equals("http://playground.dev")) {
+            throw new Exception("URL is still the default URL, check config.");
+        } else if (1 > this.getConfig().getString("key").length()) {
+            throw new Exception("Server Key is empty, this is insecure, check config.");
+        }
+    }
 
-	/**
-	 * Checks all online players against the
-	 * website's database looking for pending donation deliveries
-	 *
-	 * @return boolean
-	 * @throws Exception Why the operation failed.
-	 */
-	boolean check() throws Exception {
+    /**
+     * Gets the site URL
+     *
+     * @return URL
+     * @throws Exception Why the URL failed.
+     */
+    private URL getSiteURL() throws Exception {
+        return new URL(getConfig().getString("url") + "/wp-json/wmc/v1/server/" + getConfig().getString("key"));
+    }
 
-		// Make 100% sure the config has at least a key and url
-		this.validateConfig();
+    /**
+     * Checks all online players against the
+     * website's database looking for pending donation deliveries
+     *
+     * @return CompletableFuture<Boolean>
+     */
+    CompletableFuture<Boolean> check() {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                // Make 100% sure the config has at least a key and url
+                this.validateConfig();
 
-		// Contact the server.
-		String pendingOrders = getPendingOrders();
+                // Contact the server.
+                String pendingOrders = getPendingOrders();
 
-		// Server returned an empty response, bail here.
-		if ( pendingOrders.isEmpty() ) {
-			return false;
-		}
+                // Server returned an empty response, bail here.
+                if (pendingOrders.isEmpty()) {
+                    return false;
+                }
 
-		// Create new object from JSON response.
-		Gson gson = new GsonBuilder().create();
-		WMCPojo wmcPojo = gson.fromJson( pendingOrders, WMCPojo.class );
-		List<Order> orderList = wmcPojo.getOrders();
+                // Create new object from JSON response.
+                Gson gson = new GsonBuilder().create();
+                WMCPojo wmcPojo = gson.fromJson(pendingOrders, WMCPojo.class);
+                List<Order> orderList = wmcPojo.getOrders();
 
-		// Log if debugging is enabled.
-		wmc_log( pendingOrders );
+                // Log if debugging is enabled.
+                wmc_log(pendingOrders);
 
-		// Validate we can indeed process what we need to.
-		if ( wmcPojo.getData() != null ) {
-			// We have an error, so we need to bail.
-			wmc_log( "Code:" + wmcPojo.getCode(), 3 );
-			throw new Exception( wmcPojo.getMessage() );
-		}
+                // Validate we can indeed process what we need to.
+                if (wmcPojo.getData() != null) {
+                    // We have an error, so we need to bail.
+                    wmc_log("Code:" + wmcPojo.getCode(), 3);
+                    throw new Exception(wmcPojo.getMessage());
+                }
 
-		if ( orderList == null || orderList.isEmpty() ) {
-			wmc_log( "No orders to process.", 2 );
-			return false;
-		}
+                if (orderList == null || orderList.isEmpty()) {
+                    wmc_log("No orders to process.", 2);
+                    return false;
+                }
 
-		// foreach ORDERS in JSON feed
-		List<Integer> processedOrders = new ArrayList<>();
-		for ( Order order : orderList ) {
-			Player player = getServer().getPlayerExact( order.getPlayer() );
-			if ( null == player ) {
-				continue;
-			}
+                // foreach ORDERS in JSON feed
+                List<Integer> processedOrders = new ArrayList<>();
+                for (Order order : orderList) {
+                    Player player = getServer().getPlayerExact(order.getPlayer());
+                    if (null == player) {
+                        continue;
+                    }
 
-			// World whitelisting.
-			if ( getConfig().isSet( "whitelist-worlds" ) ) {
-				List<String> whitelistWorlds = getConfig().getStringList( "whitelist-worlds" );
-				String playerWorld = player.getWorld().getName();
-				if ( ! whitelistWorlds.contains( playerWorld ) ) {
-					wmc_log( "Player " + player.getDisplayName() + " was in world " + playerWorld + " which is not in the white-list, no commands were ran." );
-					continue;
-				}
-			}
+                    // World whitelisting.
+                    if (getConfig().isSet("whitelist-worlds")) {
+                        List<String> whitelistWorlds = getConfig().getStringList("whitelist-worlds");
+                        String playerWorld = player.getWorld().getName();
+                        if (!whitelistWorlds.contains(playerWorld)) {
+                            wmc_log("Player " + player.getDisplayName() + " was in world " + playerWorld + " which is not in the white-list, no commands were ran.");
+                            continue;
+                        }
+                    }
 
-			// Walk over all commands and run them at the next available tick.
-			for ( String command : order.getCommands() ) {
-				BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
-				scheduler.scheduleSyncDelayedTask( instance, () -> Bukkit.getServer().dispatchCommand( Bukkit.getServer().getConsoleSender(), command ), 20L );
-			}
+                    // Walk over all commands and run them at the next available tick.
+                    for (String command : order.getCommands()) {
+                        BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+                        scheduler.scheduleSyncDelayedTask(instance, () -> Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(), command), 20L);
+                    }
 
-			wmc_log( "Adding item to list - " + order.getOrderId() );
-			processedOrders.add( order.getOrderId() );
-			wmc_log( "Processed length is " + processedOrders.size() );
-		}
+                    wmc_log("Adding item to list - " + order.getOrderId());
+                    processedOrders.add(order.getOrderId());
+                    wmc_log("Processed length is " + processedOrders.size());
+                }
 
-		// If it's empty, we skip it.
-		if ( processedOrders.isEmpty() ) {
-			return false;
-		}
+                // If it's empty, we skip it.
+                if (processedOrders.isEmpty()) {
+                    return false;
+                }
 
-		// Send/update processed orders.
-		return sendProcessedOrders( processedOrders );
-	}
+                // Send/update processed orders.
+                return sendProcessedOrders(processedOrders);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+                return false;
+            }
+        });
+    }
 
-	/**
-	 * Sends the processed orders to the site.
-	 *
-	 * @param processedOrders A list of order IDs which were processed.
-	 * @return boolean
-	 */
-	private boolean sendProcessedOrders( List<Integer> processedOrders ) throws Exception {
-		// Build the GSON data to send.
-		Gson gson = new Gson();
-		WMCProcessedOrders wmcProcessedOrders = new WMCProcessedOrders();
-		wmcProcessedOrders.setProcessedOrders( processedOrders );
-		String orders = gson.toJson( wmcProcessedOrders );
+    /**
+     * Sends the processed orders to the site.
+     *
+     * @param processedOrders A list of order IDs which were processed.
+     * @return boolean
+     */
+    private boolean sendProcessedOrders(List<Integer> processedOrders) throws Exception {
+        // Build the GSON data to send.
+        Gson gson = new Gson();
+        WMCProcessedOrders wmcProcessedOrders = new WMCProcessedOrders();
+        wmcProcessedOrders.setProcessedOrders(processedOrders);
+        String orders = gson.toJson(wmcProcessedOrders);
 
-		// Setup the client.
-		OkHttpClient client = new OkHttpClient();
+        // Setup the client.
+        OkHttpClient client = new OkHttpClient();
 
-		// Process stuffs now.
-		RequestBody body = RequestBody.create( MediaType.parse( "application/json; charset=utf-8" ), orders );
-		Request request = new Request.Builder().url( getSiteURL() ).post( body ).build();
-		Response response = client.newCall( request ).execute();
+        // Process stuffs now.
+        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), orders);
+        Request request = new Request.Builder().url(getSiteURL()).post(body).build();
+        Response response = client.newCall(request).execute();
 
-		// If the body is empty we can do nothing.
-		if ( null == response.body() ) {
-			throw new Exception( "Received empty response from your server, check connections." );
-		}
+        // If the body is empty we can do nothing.
+        if (null == response.body()) {
+            throw new Exception("Received empty response from your server, check connections.");
+        }
 
-		// Get the JSON reply from the endpoint.
-		WMCPojo wmcPojo = gson.fromJson( response.body().string(), WMCPojo.class );
-		if ( null != wmcPojo.getCode() ) {
-			wmc_log( "Received error when trying to send post data:" + wmcPojo.getCode(), 3 );
-			throw new Exception( wmcPojo.getMessage() );
-		}
+        // Get the JSON reply from the endpoint.
+        WMCPojo wmcPojo = gson.fromJson(response.body().string(), WMCPojo.class);
+        if (null != wmcPojo.getCode()) {
+            wmc_log("Received error when trying to send post data:" + wmcPojo.getCode(), 3);
+            throw new Exception(wmcPojo.getMessage());
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * If debugging is enabled.
-	 *
-	 * @return boolean
-	 */
-	private boolean isDebug() {
-		return getConfig().getBoolean( "debug" );
-	}
+    /**
+     * If debugging is enabled.
+     *
+     * @return boolean
+     */
+    private boolean isDebug() {
+        return getConfig().getBoolean("debug");
+    }
 
-	/**
-	 * Gets pending orders from the WordPress JSON endpoint.
-	 *
-	 * @return String
-	 * @throws Exception On failure.
-	 */
-	private String getPendingOrders() throws Exception {
-		URL baseURL = getSiteURL();
-		BufferedReader in;
-		try {
-			in = new BufferedReader(new InputStreamReader(baseURL.openStream()));
-		} catch( FileNotFoundException e ) {
-			String msg = e.getMessage().replace( getConfig().getString( "key" ), "privateKey" );
-			throw new FileNotFoundException( msg );
-		}
+    /**
+     * Gets pending orders from the WordPress JSON endpoint.
+     *
+     * @return String
+     * @throws Exception On failure.
+     */
+    private String getPendingOrders() throws Exception {
+        URL baseURL = getSiteURL();
+        BufferedReader in;
+        try {
+            in = new BufferedReader(new InputStreamReader(baseURL.openStream()));
+        } catch (FileNotFoundException e) {
+            String msg = e.getMessage().replace(getConfig().getString("key"), "privateKey");
+            throw new FileNotFoundException(msg);
+        }
 
-		StringBuilder buffer = new StringBuilder();
+        StringBuilder buffer = new StringBuilder();
 
-		// Walk over each line of the response.
-		String line;
-		while ( ( line = in.readLine() ) != null ) {
-			buffer.append( line );
-		}
+        // Walk over each line of the response.
+        String line;
+        while ((line = in.readLine()) != null) {
+            buffer.append(line);
+        }
 
-		in.close();
+        in.close();
 
-		return buffer.toString();
-	}
+        return buffer.toString();
+    }
 
-	/**
-	 * Log stuffs.
-	 *
-	 * @param message The message to log.
-	 */
-	private void wmc_log(String message) {
-		this.wmc_log( message, 1 );
-	}
+    /**
+     * Log stuffs.
+     *
+     * @param message The message to log.
+     */
+    private void wmc_log(String message) {
+        this.wmc_log(message, 1);
+    }
 
-	/**
-	 * Log stuffs.
-	 *
-	 * @param message The message to log.
-	 * @param level The level to log it at.
-	 */
-	private void wmc_log(String message, Integer level) {
+    /**
+     * Log stuffs.
+     *
+     * @param message The message to log.
+     * @param level   The level to log it at.
+     */
+    private void wmc_log(String message, Integer level) {
 
-		if ( ! isDebug() ) {
-			return;
-		}
+        if (!isDebug()) {
+            return;
+        }
 
-		switch ( level ) {
-			case 1:
-				this.getLogger().info( message );
-				break;
-			case 2:
-				this.getLogger().warning( message );
-				break;
-			case 3:
-				this.getLogger().severe( message );
-				break;
-		}
-	}
+        switch (level) {
+            case 1:
+                this.getLogger().info(message);
+                break;
+            case 2:
+                this.getLogger().warning(message);
+                break;
+            case 3:
+                this.getLogger().severe(message);
+                break;
+        }
+    }
 }
