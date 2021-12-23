279dc811c89a34a0caa2ad21f5424a767c91faab
Jerzean <ffox259@gmail.com>
2021-12-22 22:11:42 -0500

    import PR from YouHaveTrouble add helpsite, and other commands

--- src/main/java/com/plugish/woominecraft/WooCommand.java
+++ src/main/java/com/plugish/woominecraft/WooCommand.java
@@ -5,6 +5,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,14 +23,18 @@ public class WooCommand implements TabExecutor {
 
     public WooCommand() {
         // add future subcommands here for automatic tab completion
+        subCommands.put("help", "woo.admin");
         subCommands.put("check", "woo.admin");
+        subCommands.put("ping", "woo.admin");
+        subCommands.put("debug", "woo.admin");
+
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length == 0) {
             if (sender.hasPermission("woo.admin")) {
-                sender.sendMessage(chatPrefix + plugin.getLang("general.avail_commands") + ": /woo check");
+                sender.sendMessage(chatPrefix + plugin.getLang("general.avail_commands") + ": /woo help");
             } else {
                 sender.sendMessage(chatPrefix + plugin.getLang("general.not_authorized"));
             }
@@ -35,12 +42,19 @@ public class WooCommand implements TabExecutor {
         } else if (args.length == 1) {
             if (args[0].equalsIgnoreCase("check")) {
                 checkSubcommand(sender);
-            } else {
-                sender.sendMessage(chatPrefix + "Usage: /woo check");
+            } else if (args[0].equalsIgnoreCase("ping")) {
+                pingSubcommand(sender);
+            } else if (args[0].equalsIgnoreCase("debug")) {
+                debugSubcommand(sender);
+            } else if (args[0].equalsIgnoreCase("help")) {
+                helpSubcommand(sender);
+            }
+            else {
+                sender.sendMessage(chatPrefix + "Usage: /woo help");
             }
             return true;
         }
-        sender.sendMessage(chatPrefix +"Usage: /woo check");
+        sender.sendMessage(chatPrefix +"Usage: /woo help");
         return true;
     }
 
@@ -81,5 +95,94 @@ public class WooCommand implements TabExecutor {
                 e.printStackTrace();
             }
         });
+    }
+    private void pingSubcommand(CommandSender sender) {
+        if (!sender.hasPermission("woo.admin")) {
+            String msg = chatPrefix + ChatColor.translateAlternateColorCodes('&', plugin.getLang("general.not_authorized"));
+            sender.sendMessage(msg);
+            return;
+        }
+        // Run check off the main thread
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, () -> {
+            try {
+                String msg = chatPrefix+" ";
+                sender.sendMessage(chatPrefix+"Checking connection to server");
+                HttpURLConnection ping = (HttpURLConnection) new URL(plugin.getConfig().getString("url")).openConnection();
+                ping.setConnectTimeout(700);
+                ping.setReadTimeout(700);
+                ping.setRequestMethod("HEAD");
+                int Rc = ping.getResponseCode();
+                String rs = ping.getResponseMessage();
+                ping.disconnect();
+                if (Rc < 199) {
+                    msg = chatPrefix+ChatColor.YELLOW+" Status: Ok, but possible issues, "+ Rc+" "+rs;
+                } else if (Rc >= 200 && Rc <= 299) {
+                    msg = chatPrefix+ChatColor.GREEN+" Status: Good, "+ Rc;
+                } else if (Rc >=300 && Rc <= 399) {
+                    msg = chatPrefix+ChatColor.YELLOW+" Status: Ok, but possible issues, "+ Rc+" "+rs;
+                } else if ( Rc >= 400 && Rc <=599) {
+                    msg = chatPrefix+ChatColor.DARK_RED+" Status: Bad, "+ Rc+" "+rs;
+                }
+                sender.sendMessage(msg);
+            } catch (IOException e) {
+                // send feedback for the sender
+                WooMinecraft.instance.getLogger().severe(e.getMessage());
+                sender.sendMessage(chatPrefix +ChatColor.DARK_RED+"Server Status: Failed");
+                if (plugin.isDebug()) {
+                    WooMinecraft.instance.getLogger().info(plugin.getConfig().getString("key"));
+                    WooMinecraft.instance.getLogger().info(plugin.getConfig().getString("url"));
+                }
+            }
+            try {
+                String msg = chatPrefix+" ";
+                sender.sendMessage(chatPrefix + "Checking Rest Api Url");
+                HttpURLConnection ping = (HttpURLConnection) new URL(plugin.getConfig().getString("url")+"/index.php?rest_route=/wmc/v1/server/"+ plugin.getConfig().getString("key")).openConnection();
+                ping.setConnectTimeout(700);
+                ping.setReadTimeout(700);
+                ping.setRequestMethod("HEAD");
+                int Rc = ping.getResponseCode();
+                String rs = ping.getResponseMessage();
+                ping.disconnect();
+                if (Rc < 199) {
+                    msg = chatPrefix+ChatColor.YELLOW+" Status: Ok, but possible issues, "+ Rc+" "+rs;
+                } else if (Rc >= 200 && Rc <= 299) {
+                    msg = chatPrefix+ChatColor.GREEN+" Status: Good, "+ Rc;
+                } else if (Rc >=300 && Rc <= 399) {
+                    msg = chatPrefix+ChatColor.YELLOW+" Status: Ok, but possible issues, "+ Rc+" "+rs;
+                } else if ( Rc >= 400 && Rc <=599) {
+                    msg = chatPrefix+ChatColor.DARK_RED+" Status: Bad, "+ Rc+" "+rs;
+                }
+                sender.sendMessage(msg);
+            } catch (IOException e) {
+                WooMinecraft.instance.getLogger().severe(e.getMessage());
+                sender.sendMessage(chatPrefix +ChatColor.DARK_RED+"Server Status: Failed");
+                if (plugin.isDebug()) {
+                    WooMinecraft.instance.getLogger().info(plugin.getConfig().getString("key"));
+                    WooMinecraft.instance.getLogger().info(plugin.getConfig().getString("url"));
+                }
+            }
+        });
+    }
+    private void debugSubcommand(CommandSender sender) {
+        if (plugin.getConfig().getBoolean("debug")) {
+            plugin.getConfig().set("debug", false);
+            sender.sendMessage(chatPrefix + "Debug "+ChatColor.DARK_RED+"False");
+        } else {
+            plugin.getConfig().set("debug", true);
+            sender.sendMessage(chatPrefix + "Debug "+ChatColor.GREEN+"True");
+        }
+    }
+    private void helpSubcommand(CommandSender sender) {
+        String auth = "";
+        for (int i = 0;i < plugin.getDescription().getAuthors().size()-1; i++) {
+            auth = auth+plugin.getDescription().getAuthors().get(i)+", ";
+        }
+        auth = auth+plugin.getDescription().getAuthors().get(plugin.getDescription().getAuthors().size()-1);
+        sender.sendMessage(chatPrefix +" Ver"+ plugin.getDescription().getVersion());
+        sender.sendMessage(ChatColor.DARK_PURPLE + "By " + auth);
+        sender.sendMessage(ChatColor.DARK_PURPLE + "/woo help" +ChatColor.WHITE+ " Shows this Helpsite");
+        sender.sendMessage(ChatColor.DARK_PURPLE + "/woo check" +ChatColor.WHITE+ " Check for donations/orders");
+        sender.sendMessage(ChatColor.DARK_PURPLE + "/woo ping" +ChatColor.WHITE+ " Test server connection");
+        sender.sendMessage(ChatColor.DARK_PURPLE + "/woo debug" +ChatColor.WHITE+ " Enable/disable debugging");
     }
 }
